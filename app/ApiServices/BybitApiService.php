cae5877deeed00bd9aebf60419a999d030e0ea31
pavel <pavel.bv2003@gmail.com>
2023-12-19 19:43:35 +0300

    api data received

---
+++ app/ApiServices/BybitApiService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\ApiServices;
+class BybitApiService
+{
+    private string $baseUrl = 'https://api-testnet.bybit.com/v5/market/';
+
+    public function getData(string $category, string $symbol, string $interval)
+    {
+        $url = $this->baseUrl . 'kline';
+
+        $params = [
+            'category' => $category,
+            'symbol'   => $symbol,
+            'interval' => $interval,
+        ];
+
+        return $this->buildRequest($url, $params);
+    }
+
+    private function buildRequest(string $url, array $params)
+    {
+        $curl = curl_init();
+
+        $url .= '?' . http_build_query($params);
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $response;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 
 require_once "../vendor/autoload.php";
 
+use App\ApiServices\BybitApiService;
+use App\Database\Database;
+
 $configFile = '../config/database.php';
 
 if (!file_exists($configFile)) {
@@ -10,9 +13,13 @@ if (!file_exists($configFile)) {
 
 $config = require_once $configFile;
 
-
-use App\Database\Database;
-
-print "working...";
-
 new Database($config);
+
+const CATEGORY_LINEAR = 'linear';
+const SYMBOL_BTCUSDT = 'BTCUSDT';
+const INTERVAL_5 = '5';
+
+$bybitApi = new BybitApiService();
+$res = $bybitApi->getData(CATEGORY_LINEAR, SYMBOL_BTCUSDT, INTERVAL_5);
+dd($res);
+
