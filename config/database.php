9cd0775b0a492bdc2e62637fd1e4d0395b75f004
pavel <114589142+064Pavel@users.noreply.github.com>
2023-12-22 13:46:45 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,10 +2,10 @@
 
 return [
     'driver'   =>  'mysql',
-    'host'     =>  'db',
+    'host'     =>  'localhost',
     'port'     =>  '3306',
-    'database' =>  'crypto',
-    'username' =>  'root',
-    'password' =>  'root',
+    'database' =>  'u2402254_default',
+    'username' =>  'u2402254_default',
+    'password' =>  'YF3MNg2ljEASXj42',
     'charset'  =>  'utf8'
 ];
